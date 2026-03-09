b894cbf4025c6e035ffeb365dcc6b72028c09a2b
Blood04080 <3212138067@QQ.COM>
2026-03-09 17:59:40 +0800

    chore: 添加.gitignore，移除编译生成文件

--- 8-7/8-7/method.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<stdio.h>
-double addition(double n1, double n2);
-double subtraction(double n1, double n2);
-double division(double n1, double n2);
-double multiplication(double n1, double n2);
-double get_number(void);
